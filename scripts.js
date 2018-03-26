69f0adf4ebb03f49eb65a7881df3e259ad7ef1ef
Andy Tiffany <andy.tiffany@twintechs.com>
2018-03-26 12:49:45 -0700

    should consistently use Number() to parse

--- scripts.js
+++ scripts.js
@@ -1,16 +1,15 @@
-
-function calculateTotal(){
+function calculateTotal() {
   var firstItem = $('#firstItem');
-  var secondItem = $('#secondItem');
+  var secondItem = $('#firstItem');
   var thirdItem = $('#thirdItem');
 
-  var total = parseInt(firstItem.val()) + 
-              parseInt(secnodItem.val()) + 
-              parseInt(thirdItem.val());
+  var total = Number(firstItem.val()) +
+    Number(secondItem.val()) +
+    Number(thirdItem.val());
   $('#total').text('$' + total);
 }
 
 // Review: why do we need this outer $ wrapper?
-$(function(){
+$(function () {
   $('#calculateButton').click(calculateTotal);
 })
